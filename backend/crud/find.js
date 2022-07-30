db98606f026f0a7c1900ebde957aa3e71619da3e
Marga <margalppalacios@gmail.com>
2022-07-30 11:16:47 +0200

    buscar ciudades en concreto

--- backend/crud/find.js
+++ backend/crud/find.js
@@ -3,7 +3,7 @@ require('../connection');
 const Place = require('../models/places');
 
 async function main(){
-    const places = await Place.find()
+    const places = await Place.find({name:'Madrid'})
     console.log(places)
 }
 
