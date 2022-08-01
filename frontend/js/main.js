854cfe880007f2d3bd329d8cc56e15cb0cc83dcb
Marga <margalppalacios@gmail.com>
2022-08-01 16:43:54 +0200

    TOXIC: mapa

---
+++ frontend/js/main.js
@@ -0,0 +1,26 @@
+var map = L.map('map')
+var popup = L.popup();
+
+
+map.setView([40.58893, -3.17093], 15);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
+
+L.control.scale().addTo(map);
+
+L.marker([40.30, 3.40], {
+    draggable: true
+}).addTo(map);
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
