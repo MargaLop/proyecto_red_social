777f01815b5024c24d1a583e4968d35f0e0ecd7e
Marga <margalppalacios@gmail.com>
2022-08-02 10:34:07 +0200

    mapa solucionado!!

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,26 +1,9 @@
-var map = L.map('map')
-var popup = L.popup();
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-
-map.setView([40.58893, -3.17093], 15);
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.control.scale().addTo(map);
-
-L.marker([40.30, 3.40], {
-    draggable: true
-}).addTo(map);
-
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
