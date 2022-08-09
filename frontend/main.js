9dc9ecc1ab883f9ff3bdf83af51b45bfe3d542a2
Marga <margalppalacios@gmail.com>
2022-08-09 18:31:10 +0100

    DESPLEGABLE DE COMUNIDADES

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,16 @@
-function searchClick(){
-    document.getElementById("frmsearch")
-    .submit() 
-    console.log("hiciste click");
+const placeLink = document.getElementById("tbox-place").value;
+const kmLink = document.getElementById("tbox-km").value;
+
+function searchElements(event) {
+    const dowLink = `/busqueda?place=${placeLink}&km=${kmLink}`;
+        fetch(dowLink)
+          .then((resp) => console.log(resp));
 };
 
 document
     .getElementById("btn-buscar")
     .addEventListener("click", searchElements);
 
-function searchElements(event) {
-    const placeLink = document.getElementById("tbox-place").value;
-    const kmLink = document.getElementById("tbox-km").value;
-    const dowLink = `/busqueda?place=${placeLink}&km=${kmLink}`;
-        fetch(dowLink)
-          .then((resp) => console.log(resp));
+function selectCommunities(){
+
 }
