2f6cdab4fe7d573f72c994592a5af158e8992712
Marga <margalppalacios@gmail.com>
2022-08-15 11:47:41 +0200

    mapa en españa

--- frontend/js/mapaTomtom.js
+++ frontend/js/mapaTomtom.js
@@ -1,7 +1,9 @@
-keyMap= <'Your key'>;
+keyMap= 'your key';
  
 const map = tt.map({ 
   key: keyMap, 
-  container: "map" 
+  container: "map",
+  center :{lat:39.90, lng: -4.6679915}, 
+  zoom:4.8
 }); 
 console.log(keyMap);
