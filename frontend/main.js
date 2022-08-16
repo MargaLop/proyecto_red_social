9e898426b39c783967c417f1be448072a79966be
Marga <margalppalacios@gmail.com>
2022-08-16 14:49:10 +0200

    console log de las ciudades

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,5 @@
-const placeLink = document.getElementById("tbox-place").value;
-const kmLink = document.getElementById("tbox-km").value;
-
-function searchElements(event) {
-    const dowLink = `/busqueda?place=${placeLink}&km=${kmLink}`;
-        fetch(dowLink)
-          .then((resp) => console.log(resp));
-};
-
-document
-    .getElementById("btn-buscar")
-    .addEventListener("click", searchElements);
-
-function selectCommunities(){
-    
-}
+var selectCity = document.getElementById("select-cities");
+selectCity.addEventListener('change', (event) => {
+idCity = selectCity.value
+console.log(idCity)
+});
