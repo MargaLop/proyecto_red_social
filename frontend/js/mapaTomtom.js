61b8acd85d38e89cd4789c52362af94d723b8b57
Marga <margalppalacios@gmail.com>
2022-08-17 12:15:44 +0200

    tamaño  del mapa

--- frontend/js/mapaTomtom.js
+++ frontend/js/mapaTomtom.js
@@ -1,9 +1,9 @@
-keyMap= 'your key';
+keyMap= 'NocBIAqP1CrRo5SChfQPN1OIYkkwg6VA';
  
 const map = tt.map({ 
   key: keyMap, 
   container: "map",
   center :{lat:39.90, lng: -4.6679915}, 
-  zoom:4.8
+  zoom:5
 }); 
 console.log(keyMap);
